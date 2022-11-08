aee9fffe4eb25d40072b74091ef58ae2ae5eb0b5
Quentin Marcuzzi <quentin.marcuzzi@etu.unilim.fr>
2022-11-08 21:38:25 +0100

    Successfully obtained the running game instance to access parameters

--- static/js/game/game.js
+++ static/js/game/game.js
@@ -1,54 +1,52 @@
 /**
  * Class for the game instance
  */
+
 import {Balloon} from "./balloon.js";
+
+let runningInstance;
 
 export class Game {
 
-    balloons_count;
-    min_speed;
-    max_speed;
-    amplitude;
-    frequency;
-    decision_threshold;
-    balloons;
-    score;
-    finished;
+    #min_speed;
+    #max_speed;
+    #amplitude;
+    #frequency;
+    #decision_threshold;
+    #balloons;
+    #score;
+    #requestFrameId
 
     /**
      * Constructor for the game instance
-     * @param balloons_count - the number of balloons to be created
      * @param min_speed - the minimum speed of the balloons
      * @param max_speed - the maximum speed of the balloons
      * @param amplitude - the multiplier for the cos (frequency * y) function
      * @param frequency - the multiplier for the y coordinate in the cos (frequency * y) function
      * @param decision_threshold - the threshold for which the balloon is considered to be popped in random mode
      */
-    constructor(balloons_count, min_speed, max_speed, amplitude, frequency, decision_threshold) {
-        this.balloons_count = balloons_count;
-        this.min_speed = min_speed;
-        this.max_speed = max_speed;
-        this.amplitude = amplitude;
-        this.frequency = frequency;
-        this.decision_threshold = decision_threshold;
-        this.balloons = [];
-        this.score = 0;
-        this.finished = false;
+    constructor(min_speed, max_speed, amplitude, frequency, decision_threshold) {
+        this.#min_speed = min_speed;
+        this.#max_speed = max_speed;
+        this.#amplitude = amplitude;
+        this.#frequency = frequency;
+        this.#decision_threshold = decision_threshold;
+        this.#balloons = [];
+        this.#score = 0;
+        runningInstance = this;
     }
 
-    start = function (instance = this) {
-        if (instance instanceof Game) {
-            if (instance.balloons.length < instance.balloons_count){
-                instance.balloons.push(new Balloon(0,0,0, "red"));
-                console.log(`Instantiated ${instance.balloons.length} balloons`);
-                instance.finished = (instance.balloons.length === instance.balloons_count);
-                console.log(instance.finished);
-                if (instance.finished === false) {
-                    console.log(`Calling another time`);
-                    window.requestAnimationFrame(instance.start());
-                }
-            }
-        }
+    start () {
+        console.log(runningInstance.getRequestFrameId());
+        runningInstance.setRequestFrameId(requestAnimationFrame(runningInstance.start));
+    }
+
+    getRequestFrameId () {
+        return this.#requestFrameId;
+    }
+
+    setRequestFrameId (requestFrameId) {
+        this.#requestFrameId = requestFrameId;
     }
 
 }
--- static/js/index.js
+++ static/js/index.js
@@ -4,7 +4,8 @@ import {Game} from "./game/game.js";
 // Waiting for the DOM to be loaded
 document.addEventListener("DOMContentLoaded", function () {
     // Creating the game instance
-    let game = new Game(10, 1, 5, 100, 0.01, 0.5);
-    console.log(game);
-    window.requestAnimationFrame(game.start());
+    let game = new Game(1, 5, 100, 0.01, 0.5);
+
+    // Starting the game
+    game.start();
 });
